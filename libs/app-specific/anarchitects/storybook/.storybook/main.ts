cfcbfcaf4e8f30f7c942b740cf6de023ffd21051
Johan Vrolix <johan@vrolix.eu>
2025-06-03 15:53:29 +0200

    chore:formatting

--- libs/app-specific/anarchitects/storybook/.storybook/main.ts
+++ libs/app-specific/anarchitects/storybook/.storybook/main.ts
@@ -1,7 +1,9 @@
 import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ['../../**/.@(mdx|stories.@(js|jsx|ts|tsx)), ../**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  stories: [
+    '../../**/.@(mdx|stories.@(js|jsx|ts|tsx)), ../**/*.@(mdx|stories.@(js|jsx|ts|tsx))',
+  ],
   addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
   framework: {
     name: '@storybook/angular',
