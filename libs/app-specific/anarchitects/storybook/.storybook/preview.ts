823150650e3e4cc0930c37c41f3a1409830de0c5
Johan Vrolix <johan@vrolix.eu>
2025-06-03 16:53:20 +0200

    chore: formatting

--- libs/app-specific/anarchitects/storybook/.storybook/preview.ts
+++ libs/app-specific/anarchitects/storybook/.storybook/preview.ts
@@ -1,11 +1,11 @@
-import { provideRouter } from "@angular/router";
-import { applicationConfig, Preview } from "@storybook/angular";
+import { provideRouter } from '@angular/router';
+import { applicationConfig, Preview } from '@storybook/angular';
 
 const preview: Preview = {
   decorators: [
     applicationConfig({
       providers: [provideRouter([])],
-    })
+    }),
   ],
 };
 
